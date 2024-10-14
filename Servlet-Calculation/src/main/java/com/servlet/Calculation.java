9e57c7e7b3464ba23bf19be5e9890e57c91a9ec3
prathamkumar <prathamkumar1985@gmail.com>
2024-10-14 13:38:51 +0530

    servlets calc file added

---
+++ Servlet-Calculation/src/main/java/com/servlet/Calculation.java
@@ -0,0 +1,39 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/calc")
+public class Calculation extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Calculation() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int num1 = Integer.parseInt(request.getParameter("num1"));
+		int num2 = Integer.parseInt(request.getParameter("num2"));
+		
+		response.setContentType("text/html");
+		
+		PrintWriter pw = response.getWriter();
+		pw.print(" Result is : " + (num1 + num2));
+//		System.out.println
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
