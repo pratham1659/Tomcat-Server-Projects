e9cc7346f8642bee394af55f45c3f06066c6ee4f
prathamkumar <prathamkumar1985@gmail.com>
2024-10-17 21:27:55 +0530

    servlets crud app file added

---
+++ Servlet-CrudApp/src/main/java/com/crudApp/RegisterServlet.java
@@ -0,0 +1,93 @@
+package com.crudApp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+		private final static String query = "insert into user(name,email,mobile,dob,city,gender) values(?,?,?,?,?,?)";
+		@Override
+		protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+			//get PrintWriter
+			PrintWriter pw = res.getWriter();
+			//set content type
+			res.setContentType("text/html");
+			//link the bootstrap
+			//Adding Bootstrap in Servlet
+			pw.print(" <!-- Latest compiled and minified CSS -->\n"
+					+ "<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css\">\n"
+					+ "\n"
+					+ "<!-- jQuery library -->\n"
+					+ "<script src=\"https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js\"></script>\n"
+					+ "\n"
+					+ "<!-- Popper JS -->\n"
+					+ "<script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js\"></script>\n"
+					+ "\n"
+					+ "<!-- Latest compiled JavaScript -->\n"
+					+ "<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js\"></script>\n"
+					+ "");
+			
+			//get the values
+			String name = req.getParameter("userName");
+			String email = req.getParameter("email");
+			String mobile = req.getParameter("mobile");
+			String dob = req.getParameter("dob");
+			String city = req.getParameter("city");
+			String gender = req.getParameter("gender");
+			
+			//load the JDBC driver
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			//generate the connection
+			try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/usermgmt", "root", "root@1234"	);
+					PreparedStatement ps = con.prepareStatement(query);){
+				//set the values
+				ps.setString(1, name);
+				ps.setString(2, email);
+				ps.setString(3, mobile);
+				ps.setString(4, dob);
+				ps.setString(5, city);
+				ps.setString(6, gender);
+				//execute the query
+				int count = ps.executeUpdate();
+				
+				pw.println("<div class='card' style='margin:auto;width:300px;margin-top:100px'>");
+				if(count==1) {
+					pw.println("<h2 class='bg-danger text-light text-center'>Record Registered Successfully</h2>");
+				}else {
+					pw.println("<h2 class='bg-danger text-light text-center'>Record Not Registered</h2>");
+				}
+			}catch(SQLException se) {
+				pw.println("<h2 class='bg-danger text-light text-center'>"+se.getMessage()+"</h2>");
+				se.printStackTrace();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			pw.println("<a href='index.html'><button class='btn btn-outline-success'>Home</button></a>");
+			pw.println("</div>");
+			//close the stream
+			pw.close();
+		}
+		@Override
+		protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+			doGet(req,res);
+	}
+}
+
+
+
